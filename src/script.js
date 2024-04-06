412f213136a966711dcb647aac4fdd305937c196
Ma-Lucy <161073044+Ma-Lucy@users.noreply.github.com>
2024-04-06 10:08:25 +0100

    Deleted Oslo. Created a function to update London time

--- src/script.js
+++ src/script.js
@@ -12,3 +12,18 @@ function updateHongkongSeconds() {
 }
 
 setInterval(updateHongkongSeconds, 1000);
+
+function updateLondonSeconds() {
+  let londonCityElement = document.querySelector("#london");
+  let londonDateElement = document.querySelector("#londonDate");
+  let londonTimeElement = document.querySelector("#londonTime");
+
+  londonDate = moment().tz("Europe/London").format("dddd Do MMMM");
+  londonDateElement.innerHTML = londonDate;
+  londonTime = moment()
+    .tz("Europe/London")
+    .format("h:mm:ss [<small>]A[</small>]");
+  londonTimeElement.innerHTML = londonTime;
+}
+
+setInterval(updateLondonSeconds, 1000);
