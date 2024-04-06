4d33043d64b278cecb6f0ca3687e823720bffa01
Ma-Lucy <161073044+Ma-Lucy@users.noreply.github.com>
2024-04-06 11:39:33 +0100

    Created a funtion to target the select menu cities

--- src/script.js
+++ src/script.js
@@ -1,29 +1,53 @@
-function updateHongkongSeconds() {
+function updateSeconds() {
   let hongkongCityElement = document.querySelector("#hongkong");
-  let hongkongDateElement = document.querySelector(".date");
-  let hongkongTimeElement = document.querySelector(".time");
+  if (hongkongCityElement) {
+    let hongkongDateElement = document.querySelector(".date");
+    let hongkongTimeElement = document.querySelector(".time");
 
-  hongkongDate = moment().tz("Asia/Hong_Kong").format("dddd Do MMMM");
-  hongkongDateElement.innerHTML = hongkongDate;
-  hongkongTime = moment()
-    .tz("Asia/Hong_Kong")
-    .format("h:mm:ss [<small>]A[</small>]");
-  hongkongTimeElement.innerHTML = hongkongTime;
-}
+    hongkongDate = moment().tz("Asia/Hong_Kong").format("dddd Do MMMM");
+    hongkongDateElement.innerHTML = hongkongDate;
+    hongkongTime = moment()
+      .tz("Asia/Hong_Kong")
+      .format("h:mm:ss [<small>]A[</small>]");
+    hongkongTimeElement.innerHTML = hongkongTime;
+  }
 
-setInterval(updateHongkongSeconds, 1000);
-
-function updateLondonSeconds() {
   let londonCityElement = document.querySelector("#london");
-  let londonDateElement = document.querySelector("#londonDate");
-  let londonTimeElement = document.querySelector("#londonTime");
+  if (londonCityElement) {
+    let londonDateElement = document.querySelector("#londonDate");
+    let londonTimeElement = document.querySelector("#londonTime");
 
-  londonDate = moment().tz("Europe/London").format("dddd Do MMMM");
-  londonDateElement.innerHTML = londonDate;
-  londonTime = moment()
-    .tz("Europe/London")
-    .format("h:mm:ss [<small>]A[</small>]");
-  londonTimeElement.innerHTML = londonTime;
+    londonDate = moment().tz("Europe/London").format("dddd Do MMMM");
+    londonDateElement.innerHTML = londonDate;
+    londonTime = moment()
+      .tz("Europe/London")
+      .format("h:mm:ss [<small>]A[</small>]");
+    londonTimeElement.innerHTML = londonTime;
+  }
 }
 
-setInterval(updateLondonSeconds, 1000);
+function selectCityFunction(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let newCityDate = moment().tz(cityTimezone).format("MMMM Do YYYY");
+  let newCityTime = moment()
+    .tz(cityTimezone)
+    .format("h:mm:ss [<small>]A[</small>]");
+  let newCityElement = document.querySelector("#cities");
+  newCityElement.innerHTML = `<div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${newCityDate}</div>
+        </div>
+        <div class="time">
+          ${newCityTime}
+        </div>
+      </div>
+    `;
+}
+
+let selectDrop = document.querySelector("#select-drop");
+selectDrop.addEventListener("change", selectCityFunction);
+
+updateSeconds();
+setInterval(updateSeconds, 1000);
